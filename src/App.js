79d5081d634a58543c7131a404814bc05efaed17
Chaimae <chaima.aberji@gmail.com>
2024-09-05 04:17:59 +0700

    spell pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import Dashboard from "./pages/dashboard.js";
 import Card from "./components/card.js";
 import Characters from "./pages/Characters";
+import Spells from "./pages/Spells";
 
 function App() {
   return (
     <div className="App">
-      <Characters />
+      <Spells />
     </div>
   );
 }
---
+++ src/pages/Spells.js
@@ -0,0 +1,40 @@
+import Header from "../components/header";
+import "./Spells.css";
+import React, { useEffect, useState } from "react";
+import Spell from "../components/spell.js";
+
+function Spells() {
+  const [spells, setSpells] = useState([]);
+  const [span, setSpan] = useState(1);
+
+  useEffect(() => {
+    fetch("https://hp-api.onrender.com/api/spells")
+      .then((response) => response.json())
+      .then((data) => setSpells(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  const handleLoadMore = () => {
+    setSpan(span + 1);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="grid-container">
+        {spells.map((item, index) =>
+          index < 9 * span ? (
+            <Spell name={item.name} description={item.description} />
+          ) : (
+            <></>
+          )
+        )}
+        <button className="load" onClick={handleLoadMore}>
+          Load More
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Spells;
