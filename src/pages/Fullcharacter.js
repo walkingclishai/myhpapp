77ee53daa2f91bf1419e7319ec27e0684b438c2c
Chaimae <chaima.aberji@gmail.com>
2024-09-08 03:12:14 +0700

    full character page

--- src/pages/Fullcharacter.js
+++ src/pages/Fullcharacter.js
@@ -1,6 +1,7 @@
 import Header from "../components/header";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import "./Fullcharacter.css";
 
 function Fullcharacter() {
   const location = useLocation();
@@ -20,7 +21,61 @@ function Fullcharacter() {
       <Header />
 
       {characters.map((item) =>
-        item.id === info ? <h1>{item.name}</h1> : <></>
+        item.id === info ? (
+          <div className="full">
+            <div className="img-container">
+              <img className="char-logo" src={item.image} />
+              <img
+                className="banner"
+                src={
+                  item.house == "Gryffindor"
+                    ? "/banner 2.png"
+                    : item.house == "Ravenclaw"
+                    ? "/banner 4.png"
+                    : item.house == "Hufflepuff"
+                    ? "/banner 3.png"
+                    : "/banner 1.png"
+                }
+              />
+            </div>
+
+            <table>
+              <thead></thead>
+              <tbody>
+                <tr className={item.house}>
+                  <td className="label">Name:</td>
+                  <td>{item.name}</td>
+                </tr>
+                <tr className={item.house}>
+                  <td className="label">Gender:</td>
+                  <td>{item.gender}</td>
+                </tr>
+                <tr className={item.house}>
+                  <td className="label">Date of birth:</td>
+                  <td>{item.dateOfBirth}</td>
+                </tr>
+                <tr className={item.house}>
+                  <td className="label">Ancestry:</td>
+                  <td>{item.ancestry}</td>
+                </tr>
+                <tr className={item.house}>
+                  <td className="label">Patronus:</td>
+                  <td>{item.patronus}</td>
+                </tr>
+                <tr className={item.house}>
+                  <td className="label">Position at Hogwarts:</td>
+                  <td>{item.hogwartsStudent == true ? "Student" : "Staff"}</td>
+                </tr>
+                <tr className={item.house}>
+                  <td className="label">Played by:</td>
+                  <td>{item.actor}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <></>
+        )
       )}
     </main>
   );
